3fd21eb45e65e4167fbb336328800f6c8496747e
GianpieroTulipani <50196821+GianpieroTulipani@users.noreply.github.com>
2025-01-23 11:54:19 +0100

    changed test.py

--- smilesmvp/test.py
+++ smilesmvp/test.py
@@ -1,9 +1,10 @@
 import pandas as pd
-from smilesmvp.config import PROCESSED_DATA_DIR
+#from smilesmvp.config import PROCESSED_DATA_DIR
 from smilesmvp.dataset import Molecule3DMaskingDataset, collate_fn
 from torch.utils.data import DataLoader
 
-root_3d = f"{PROCESSED_DATA_DIR}/GEOM_3D_nmol50000_nconf5_nupper1000"
+root_3d = f'kaggle/input/geom-3d-nmol50000-nconf5-nupper1000'
+#root_3d = f"{PROCESSED_DATA_DIR}/GEOM_3D_nmol50000_nconf5_nupper1000"
 csv_path = f"{root_3d}/processed/smiles.csv"
 
 print("Loading 3D dataset...")
