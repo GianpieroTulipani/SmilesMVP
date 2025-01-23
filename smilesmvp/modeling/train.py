1789a4c14a4bf685ef149cedde64c65d8e23de98
GianpieroTulipani <50196821+GianpieroTulipani@users.noreply.github.com>
2025-01-23 13:06:08 +0100

    changed train.py

--- smilesmvp/modeling/train.py
+++ smilesmvp/modeling/train.py
@@ -1,17 +1,21 @@
 from loguru import logger
 
+import sys
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from ..config import args
-from ..models.vae import VariationalAutoEncoder
-from ..models.schnet import SchNet
+sys.path.append('/kaggle/working/SmilesMVP/smilesmvp')
+sys.path.append('/kaggle/working/SmilesMVP/smilesmvp/modeling')
+sys.path.append('/kaggle/working/SmilesMVP/smilesmvp/models')
+from config import args
+from models.vae import VariationalAutoEncoder
+from models.schnet import SchNet
 #from torch_geometric.data import DataLoader
 from torch.utils.data import DataLoader
 from tqdm import tqdm
-from ..modeling.contrastive_loss import dual_CL
+from modeling.contrastive_loss import dual_CL
 from dataset import Molecule3DMaskingDataset, collate_fn
 from transformers import AutoModel, AutoTokenizer
 
